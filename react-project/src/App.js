fb2235b0cb8b8dfb6289e953578278d9d276c45a
gwagner-2020 <74742624+gwagner-2020@users.noreply.github.com>
2024-08-17 19:26:13 -0500

    Api call to v1-artworks

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [artworks, setArtworks] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://api.artic.edu/api/v1/artworks')
+    .then(response => {
+        setArtworks(response.data);
+      }
+    )
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+  console.log(artworks.pagination);
+  console.log(artworks.data);
+  const artworksArray = artworks.data;
+  console.log('artworksArray:', artworksArray);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <div>
+      <ul>
+      {artworksArray.map(artwork=> (
+        <li key={artwork.id}>{artwork.artist_display}, {artwork.title}</li>
+      ))}
+      </ul>
     </div>
   );
 }
