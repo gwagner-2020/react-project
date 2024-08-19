ced14e2b26de4fa2ee0d92578b3463559a20921b
gwagner-2020 <74742624+gwagner-2020@users.noreply.github.com>
2024-08-18 20:43:29 -0500

    Increase pagination and add images

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
@@ -6,10 +5,55 @@ import axios from 'axios';
 function App() {
   const [artworks, setArtworks] = useState([]);
 
-  useEffect(() => {
-    axios.get('https://api.artic.edu/api/v1/artworks')
-    .then(response => {
-        setArtworks(response.data);
+//   const baseUrl: 'https://api.worldbank.org/v2/country/'
+
+// const getIndicatorByCountry = async (country: string, indicator: string, page:number=1): Promise<[]> => {  
+//   const query = `${baseUrl}/${country}/indicator/${indicator}?page=${page}&format=json`
+//   const response = await axios.get(query)  
+//   const data = response.data
+
+//   if (data[0].pages > page) {
+//     return data.concat(await getIndicatorByCountry(country, indicator, page+1)) 
+//   } else {
+//     return data
+//   }
+// }
+
+
+// const instance = axios.create({
+//   baseURL: 'https://api.artic.edu/api',
+// });
+
+// getAllArtworks();
+
+// async function getAllArtworks() {
+//     try {
+//         const response = await instance.get('/v1/artworks');
+//         const totalArtworks = response.data.count;
+//         const totalPages = Math.ceil(totalArtworks / response.data.max_per_page);
+//         const promiseArray = [];
+//         for (let i = 0; i < (totalPages + 1); i++) {
+//             promiseArray.push(instance.get(`/artworks?page=${i}`));
+//         };
+
+//         // promise.all allows you to make multiple axios requests at the same time.
+//         // It returns an array of the results of all your axios requests
+//         let resolvedPromises = await Promise.all(promiseArray)
+//         for (let i = 0; i < resolvedPromises.length; i++) {
+//             // This will give you access to the output of each API call
+//             console.log(resolvedPromises[i])
+//         }
+//         setArtworks(resolvedPromises);
+//     } catch (err) {
+//         console.log('Something went wrong.');
+//     }
+//   }
+  
+    useEffect(() => {
+    // getAllArtworks();
+    axios.get('https://api.artic.edu/api/v1/artworks??page=1&limit=100')
+    .then(response => {      
+      setArtworks(response.data);
       }
     )
       .catch(error => {
@@ -17,35 +61,25 @@ function App() {
       });
   }, []);
 
-  console.log(artworks.pagination);
-  console.log(artworks.data);
+  //console.log(artworks.pagination);
+  //console.log(artworks.data);
   const artworksArray = artworks.data;
   console.log('artworksArray:', artworksArray);
 
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     <div>
       <ul>
-      {artworksArray.map(artwork=> (
-        <li key={artwork.id}>{artwork.artist_display}, {artwork.title}</li>
+      {artworksArray?.map(artwork=> (
+        <><li>{artwork?.artist_display}, {artwork?.title}</li><li>
+          <img src={'https://www.artic.edu/iiif/2/' + artwork?.image_id + '/full/200,/0/default.jpg'} alt="artwork" />
+        </li></>
       ))}
       </ul>
+      {/* {artworksArray?.map(artwork => (
+      <img key={artwork?.id} src={'https://www.artic.edu/iiif/2/' + artwork?.image_id + '/full/843,/0/default.jpg'} alt="artwork"></img>
+    ))} */}
     </div>
+
   );
 }
 
